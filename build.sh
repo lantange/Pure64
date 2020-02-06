ea5283f2c49e776460fd3b8d33d0ccb42e47dd34
Eric <42959685+lantange@users.noreply.github.com>
2020-02-06 10:17:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ mkdir -p bin
 
 cd src
 
-nasm pure64.asm -o ../bin/pure64.sys
+nasm pure64.asm -o ../bin/pure64.sys -l ../bin/pure64-debug.txt
 
 cd bootsectors
 
-nasm mbr.asm -o ../../bin/mbr.sys
+nasm mbr.asm -o ../../bin/mbr.sys -l ../bin/mbr-debug.txt
 nasm pxestart.asm -o ../../bin/pxestart.sys
 nasm multiboot.asm -o ../../bin/multiboot.sys
 nasm multiboot2.asm -o ../../bin/multiboot2.sys
